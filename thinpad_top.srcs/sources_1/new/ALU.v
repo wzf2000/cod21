69045408afd52c501a2b79318ab782913d9dcdb4
wzf2000 <nbjcwzf@163.com>
2021-10-04 21:00:58 +0800

    Fix3

--- thinpad_top.srcs/sources_1/new/ALU.v
+++ thinpad_top.srcs/sources_1/new/ALU.v
@@ -2,7 +2,7 @@ module ALU(
     input wire clock_btn,
     input wire reset_btn,
     input wire [31:0] dip_sw,
-    output wire [15:0] leds
+    output reg [15:0] leds
 );
 
 reg[1:0] ALU_state;
@@ -11,62 +11,66 @@ reg signed[15:0] ALU_B;
 reg ALU_exc;
 
 initial begin
-    ALU_exc = 1'b0;
-end
-initial begin
     ALU_state <= 2'b0;
 end
 
 always@(*) begin
-    if (ALU_state == 2'b10)
-        case (dip_sw[3:0])
-            4'd1: begin // ADD
-                {ALU_exc, leds} = ALU_A + ALU_B;
-            end
-            4'd2: begin // SUB
-                {ALU_exc, leds} = ALU_A - ALU_B;
-            end
-            4'd3: begin // AND
-                leds = ALU_A & ALU_B;
-                ALU_exc = 0;
-            end
-            4'd4: begin // OR
-                leds = ALU_A | ALU_B;
-                ALU_exc = 0;
-            end
-            4'd5: begin // XOR
-                leds = ALU_A ^ ALU_B;
-                ALU_exc = 0;
-            end
-            4'd6: begin // NOT
-                leds = ~ALU_A;
-                ALU_exc = 0;
-            end
-            4'd7: begin // SLL
-                leds = ALU_A <<< ALU_B;
-                ALU_exc = 0;
-            end
-            4'd8: begin // SRL
-                leds = ALU_A >>> ALU_B;
-                ALU_exc = 0;
-            end
-            4'd8: begin // SRA
-                leds = ALU_A >> ALU_B;
-                ALU_exc = 0;
-            end
-            4'd10: begin // ROL
-                leds = ALU_A <<< ALU_B | ALU_A >>> (16 - ALU_B);
-                ALU_exc = 0;
-            end
-            default: begin
-                leds = 0;
-                ALU_exc = 0;
-            end
-        endcase
-    else begin
-        leds = 0;
-        ALU_exc = 0;
-    end
+    case (ALU_state)
+        2'b10:
+            case (dip_sw[3:0])
+                4'd1: begin // ADD
+                    {ALU_exc, leds} <= ALU_A + ALU_B;
+                end
+                4'd2: begin // SUB
+                    {ALU_exc, leds} <= ALU_A - ALU_B;
+                end
+                4'd3: begin // AND
+                    leds <= ALU_A & ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd4: begin // OR
+                    leds <= ALU_A | ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd5: begin // XOR
+                    leds <= ALU_A ^ ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd6: begin // NOT
+                    leds <= ~ALU_A;
+                    ALU_exc <= 0;
+                end
+                4'd7: begin // SLL
+                    leds <= ALU_A <<< ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd8: begin // SRL
+                    leds <= ALU_A >>> ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd8: begin // SRA
+                    leds <= ALU_A >> ALU_B;
+                    ALU_exc <= 0;
+                end
+                4'd10: begin // ROL
+                    leds <= ALU_A <<< ALU_B | ALU_A >>> (16 - ALU_B);
+                    ALU_exc <= 0;
+                end
+                default: begin
+                    leds <= 0;
+                    ALU_exc <= 0;
+                end
+            endcase
+        2'b11: begin
+            leds[0] <= ALU_exc;
+            leds[15:1] <= 0;
+            ALU_exc <= ALU_exc;
+        end
+        default: begin
+            leds <= 0;
+            ALU_exc <= 0;
+        end
+    endcase
 end
 
 always@(posedge clock_btn or posedge reset_btn) begin
@@ -78,17 +82,15 @@ always@(posedge clock_btn or posedge reset_btn) begin
         case (ALU_state)
             2'b00: begin
                 ALU_A <= dip_sw[15:0];
-                // leds = dip_sw[15:0];
+                // leds <= dip_sw[15:0];
             end
             2'b01: begin
                 ALU_B <= dip_sw[15:0];
-                // leds = dip_sw[15:0];
+                // leds <= dip_sw[15:0];
             end
             2'b10: begin
-                
             end
             2'b11: begin
-                leds[0] <= ALU_exc;
             end
         endcase
     end
